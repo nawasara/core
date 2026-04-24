cb5b1da6798cd8b703602d8f8dec5a2f2d699997
Pringgo J. Saputro <odyinggo@gmail.com>
2026-04-24 13:50:29 +0700

    fix(menu): replace heroicon references with lucide icons

--- config/menu.php
+++ config/menu.php
@@ -5,20 +5,20 @@ $prefix = 'nawasara-core';
 return [
     [
         'label' => 'User Management',
-        'icon' => 'heroicon-o-users',
+        'icon' => 'lucide-users',
         'url' => '',
         'permission' => $prefix.'.user.view',
         'submenu' => [
             [
                 'label' => 'Users',
-                'icon' => 'heroicon-o-list-bullet',
+                'icon' => 'lucide-user',
                 'url' => url($prefix.'/users'),
                 'permission' => $prefix.'.user.view',
                 'navigate' => true,
             ],
             [
                 'label' => 'Role',
-                'icon' => 'heroicon-o-key',
+                'icon' => 'lucide-shield-check',
                 'url' => url($prefix.'/roles'),
                 'permission' => $prefix.'.role.view',
                 'navigate' => true,
